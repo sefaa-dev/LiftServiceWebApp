2f2afcf5f4be82d56fb50858b45c4d723b135158
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-02-11 23:19:07 +0300

    Dev Extreme Path

--- LiftServiceWebApp/Startup.cs
+++ LiftServiceWebApp/Startup.cs
@@ -5,12 +5,15 @@ using LiftServiceWebApp.Models.Identity;
 using LiftServiceWebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 
 namespace LiftServiceWebApp
 {
@@ -75,6 +78,12 @@ namespace LiftServiceWebApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/vendor")
+            });
 
             app.UseRouting();
 
