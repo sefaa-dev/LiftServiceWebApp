d7fb2e7902240e951ed405e24f3ad147efad49d5
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-02-11 22:05:59 +0300

    Subcription Model Created

---
+++ LiftServiceWebApp/Entities/Subscription.cs
@@ -0,0 +1,27 @@
+﻿using LiftServiceWebApp.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiftServiceWebApp.Entities
+{
+    public class Subscription : BaseEntity
+    {
+        public Guid SubscriptionTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime EndDate { get; set; }
+        [StringLength(450)]
+        public string UserId { get; set; }
+        [NotMapped]
+        public bool IsActive => EndDate > DateTime.Now;
+
+        [ForeignKey(nameof(SubscriptionTypeId))]
+        public virtual SubscriptionType SubscriptionType { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ LiftServiceWebApp/Entities/SubscriptionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiftServiceWebApp.Entities
+{
+    public class SubscriptionType : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Month { get; set; }
+        public decimal Price { get; set; }
+    }
+}
