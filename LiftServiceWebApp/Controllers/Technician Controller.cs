cb3b66568739d66dc67116ded5528906d54f95fa
sefaa-dev <sefayilmaz582@gmail.com>
2022-02-16 15:01:29 +0300

    add technician controller

---
+++ LiftServiceWebApp/Controllers/Technician Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiftServiceWebApp.Controllers
+{
+    public class Technician_Controller : Controller
+    {
+        public IActionResult Failures()
+        {
+            return View();
+        }
+    }
+}
