86fc2fe4748c42ec29f3f47bdbad3cba0d83eb60
Mehmet Salih ATKAN <mehmetsalihatkan@gmail.com>
2022-03-02 00:02:57 +0300

    failureeupdate's map marking fixed

--- LiftServiceWebApp/wwwroot/user-assets/js/harita.js
+++ LiftServiceWebApp/wwwroot/user-assets/js/harita.js
@@ -1,49 +1,57 @@
 function konumBul(position) {
     console.log(position);
-    if (position == null) {
-        position.lat = parseFloat(position.lat);
-        position.lng = parseFloat(position.lng);
+    if (position !== null) {
 
-        //harita iþaretlenecek
-        var map = new google.maps.Map($('#map')[0], {
-            zoom: 15,
-            //center: new google.maps.LatLng(40.747688, -74.004142),
-            center: position,
+        $(document).ready(function () {
 
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            var map = new google.maps.Map($('#map')[0], {
+                zoom: 15,
+                //center: new google.maps.LatLng(40.747688, -74.004142),
+                center: position,
+
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            var marker = new google.maps.Marker({
+                position: position,
+                map,
+                title: 'Arýza Konum',
+            });
+
+            google.maps.event.addListener(map, 'click', function (e) {
+                if (marker == null) {
+                    marker = new google.maps.Marker({
+                        position: e["latLng"],
+                        title: "Hello World!"
+                    });
+
+                }
+                else {
+                    marker.setMap(null);
+                    marker = new google.maps.Marker({
+                        position: e["latLng"],
+                        title: "Hello World!"
+                    });
+                }
+                marker.setMap(map);
+                console.log(marker.getPosition().lat());
+                console.log(marker.getPosition().lng());
+                document.getElementById("lat").value = marker.getPosition().lat();
+                document.getElementById("lng").value = marker.getPosition().lng();
+            });
+
+            /*console.log(marker.getPosition().lat());*/
+
         });
-        var marker;
-        google.maps.event.addListener(map, 'click', function (e) {
-            if (marker == null) {
-                marker = new google.maps.Marker({
-                    position: position,
-                    title: "Hello World!"
-                });                
-            }
-            else {
-                marker.setMap(null);
-                marker = new google.maps.Marker({
-                    position: position,
-                    title: "Hello World!"
-                });
-            }
-            marker.setMap(map);
-            console.log(marker.getPosition().lat());
-            console.log(marker.getPosition().lng());
-            document.getElementById("lat").value = marker.getPosition().lat();
-            document.getElementById("lng").value = marker.getPosition().lng();
-        });
-    }
-    else {
+    } else {
         if (navigator.geolocation) {
-            console.log(); ('Geolocation destekliyor')
+            console.log('Geolocation destekliyor');
             navigator.geolocation.getCurrentPosition(function (data) {
                 console.log(data);
-                var directionsService = new google.maps.DirectionsService();
-                var directionsRenderer = new google.maps.DirectionsRenderer();
 
                 const konum = {
                     lat: data.coords.latitude,
+                    //lat: lat,
+                    //lng: lng,
                     lng: data.coords.longitude
                 };
 
@@ -53,6 +61,7 @@ function konumBul(position) {
                         zoom: 15,
                         //center: new google.maps.LatLng(40.747688, -74.004142),
                         center: konum,
+
                         mapTypeId: google.maps.MapTypeId.ROADMAP
                     });
                     var marker;
@@ -71,11 +80,14 @@ function konumBul(position) {
                             });
                         }
                         marker.setMap(map);
+                        console.log(marker.getPosition().lat());
+                        console.log(marker.getPosition().lng());
                         document.getElementById("lat").value = marker.getPosition().lat();
                         document.getElementById("lng").value = marker.getPosition().lng();
-                        console.log("lat", marker.getPosition().lat());
-                        console.log("lng", marker.getPosition().lng());
                     });
+
+                    /*console.log(marker.getPosition().lat());*/
+
                 });
 
             }, function (error) {
@@ -84,7 +96,7 @@ function konumBul(position) {
         } else {
             alert('Geolocation desteklemiyor')
         }
-
     }
+
+
 }
-//konumBul();
