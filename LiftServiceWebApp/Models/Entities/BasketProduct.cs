6ca701965c9d23b726453ff85609bd5e9a2acd70
Fatih Gören <fatihgoren61@gmail.com>
2022-03-20 03:40:51 +0300

    Payment cart design fixed

--- LiftServiceWebApp/Models/Entities/BasketProduct.cs
+++ LiftServiceWebApp/Models/Entities/BasketProduct.cs
@@ -5,13 +5,19 @@ namespace LiftServiceWebApp.Models.Entities
 {
     public class BasketProduct
     {
+        // Basket Id
         public Guid BasketId { get; set; }
         [ForeignKey(nameof(BasketId))]
         public virtual Basket Basket { get; set; }
 
+
+        // Product Id
         public Guid ProductId { get; set; }
         [ForeignKey(nameof(ProductId))]
         public virtual Product Product { get; set; }
+
+
+        // Product -> Quantity and Price
         public int Quantity { get; set; } = 1;
         public decimal Price { get; set; }
     }
